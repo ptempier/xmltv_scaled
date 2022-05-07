a1568d5cd44374f1fb212b3b1bf3004ff584219c
Pascal Tempier <ptempier@users.noreply.github.com>
2022-05-07 19:52:15 +0200

    Update importxmltv.sh

--- importxmltv.sh
+++ importxmltv.sh
@@ -3,24 +3,27 @@
 GRABBER="tv_grab_fr_telerama"
 BASE="/data"
 DATA="$BASE/xmldata"
-GRAB_OPTS="--no_aggregatecat -delay 5"
+GRAB_OPTS="--no_aggregatecat -delay 5 --casting"
+GRAB_CFG="/data/tv_grab_fr_telerama_tnt.config"
 
-#to be more secure , need to name  files with 
+
+#to be more secure, need to name  files with 
 #date -d "+ 10 days" +"%Y %m %d"
 
 function rotate_files {
 	
 	echo "Rotating files"
-	mv "$DATA/$GRABBER.xml.10" "$DATA/$GRABBER.xml.09"
-	mv "$DATA/$GRABBER.xml.09" "$DATA/$GRABBER.xml.08"
-	mv "$DATA/$GRABBER.xml.08" "$DATA/$GRABBER.xml.07"
-	mv "$DATA/$GRABBER.xml.07" "$DATA/$GRABBER.xml.06"
-	mv "$DATA/$GRABBER.xml.06" "$DATA/$GRABBER.xml.05"
-	mv "$DATA/$GRABBER.xml.05" "$DATA/$GRABBER.xml.04"
-	mv "$DATA/$GRABBER.xml.04" "$DATA/$GRABBER.xml.03"
-	mv "$DATA/$GRABBER.xml.03" "$DATA/$GRABBER.xml.02"
-	mv "$DATA/$GRABBER.xml.02" "$DATA/$GRABBER.xml.01"
 	mv "$DATA/$GRABBER.xml.01" "$DATA/$GRABBER.xml.00"
+	mv "$DATA/$GRABBER.xml.02" "$DATA/$GRABBER.xml.01"
+	mv "$DATA/$GRABBER.xml.03" "$DATA/$GRABBER.xml.02"
+	mv "$DATA/$GRABBER.xml.04" "$DATA/$GRABBER.xml.03"
+	mv "$DATA/$GRABBER.xml.05" "$DATA/$GRABBER.xml.04"
+	mv "$DATA/$GRABBER.xml.06" "$DATA/$GRABBER.xml.05"
+	mv "$DATA/$GRABBER.xml.07" "$DATA/$GRABBER.xml.06"
+	mv "$DATA/$GRABBER.xml.08" "$DATA/$GRABBER.xml.07"
+	mv "$DATA/$GRABBER.xml.09" "$DATA/$GRABBER.xml.08"
+	mv "$DATA/$GRABBER.xml.10" "$DATA/$GRABBER.xml.09"
+
 }
 
 function run_if_older {
@@ -37,12 +40,14 @@ function run_if_older {
 	NOW=$(( $(date +"%s" ) ))
 	FDATE=$(stat -c "%Y" "$FILE")
 
-	if  [[ ! -f "$FILE" ]] ; then FDATE=0 ; fi
+	if  [[ ! -f "$FILE" ]] ; then FDATE=0 ; echo "Missing file $FILE"; fi
+	if  [[ ! -s "$FILE" ]] ; then FDATE=0 ; echo "Empty   file $FILE"; fi
+
 
 	if [[ "$(( $NOW - $FDATE ))" -gt "$AGE" ]]
        	then
 		echo "'$FILE' is older than $DAYS days, updating"
-		"$BASE/$GRABBER" --config-file "$BASE/$GRABBER.config"  --days 1 --offset "$OFFSET" $GRAB_OPTS   --output "$FILE"
+		"$BASE/$GRABBER" --config-file "$GRAB_CFG"  --days 1 --offset "$OFFSET" $GRAB_OPTS   --output "$FILE"
 		cat "$FILE" | socat - "/tvheadend/epggrab/xmltv.sock"
 
 		if  [[ "$OFFSET" = "02" ]] ;then rotate_files ; fi
